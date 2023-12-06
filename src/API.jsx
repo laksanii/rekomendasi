c391295faf1a4aca573bdf65119a5a934a913763
unknown <prazetyo111@gmail.com>
2023-12-06 21:28:42 +0700

    change: update Flask API URL to Prod

--- src/API.jsx
+++ src/API.jsx
@@ -1,7 +1,7 @@
 export const RAJA_ONGKIR_API_KEY = "05fb707215b778a76401b6996bc53823";
 export const RAJA_ONGKIR_GET_PROVINCE_URL =
     "https://api.rajaongkir.com/starter/province";
-export const FLASK_API_URL = "http://127.0.0.1:5000";
+export const FLASK_API_URL = "https://flask-api.sunnycos-rent.my.id";
 export const FLASK_GET_COSTUME_URL = "/costume";
 export const FLASK_PROCESS_URL = "/process";
 export const FLASK_DETAIL_URL = "/detail";
